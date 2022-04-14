76791f845b17ec0523d4b155c45593e5439ee740
AlbertGk <albertgk01@gmail.com>
2022-04-14 07:58:48 +0200

    update date time section

--- src/partials-js/date_time_section.js
+++ src/partials-js/date_time_section.js
@@ -1,12 +1,19 @@
 import './sass/main.scss';
 import { getCityWeather } from './partials-js/data_fetch';
+import moment from 'moment';
 
 const cityDateTag = document.querySelector('section.city-date');
 
 console.log(response.city.sunrise);
 console.log(response.city.sunset);
 
-function unixTimeConverter(unixTime) {
+function shiftInSecondsConverter(secondsFromUTC, desiredFormat) {
+  const timeInMinutes = secondsFromUTC / 60;
+  const currTime = moment().utcOffset(timeInMinutes).format(`${desiredFormat}`);
+  return currTime;
+}
+
+function unixTimeConverterHhMm(unixTime) {
   let date = new Date(unixTime * 1000);
   let hours = date.getHours();
   let minutes = '0' + date.getMinutes();
@@ -17,23 +24,23 @@ function unixTimeConverter(unixTime) {
   return formattedTime;
 }
 
-unixTimeConverter(response.city.sunrise);
+unixTimeConverterHhMm(response.city.sunrise);
 
-function createGalleryTags(backendObjects, callback) {
+function createGalleryTags(backendObjects, callback1, callback2) {
   const markup = backendObjects
     .map(
       backendObj =>
         `<div class = "city-date__container">
             <div class = "city-date__day">
-                <p class = "">8th Sat</p>
+                <p class = "">${(callback1(backendObjects.city.timezone), 'Do')}</p>
             </div>
             <div class = "city-date__month-and-time">
-                <p class = "">February</p>
-                <p class = "">21:30:56</p>
+                <p class = "">${(callback1(backendObjects.city.timezone), 'MMMM')}</p>
+                <p class = "">${(callback1(backendObjects.city.timezone), 'h:mm:ss')}</p>
             </div>
             <div class = "city-date__sunrise-and-sunset">
-                <p class = "">${callback(backendObjects.city.sunrise)}</p>
-                <p class = "">${callback(backendObjects.city.sunset)}</p>
+                <p class = "">${callback2(backendObjects.city.sunrise)}</p>
+                <p class = "">${callback2(backendObjects.city.sunset)}</p>
             </div>
         </div>`,
     )
@@ -41,4 +48,4 @@ function createGalleryTags(backendObjects, callback) {
   cityDateTag.insertAdjacentHTML('beforeend', markup);
 }
 
-createGalleryTags(response, unixTimeConverter);
+createGalleryTags(response, unixTimeConverterHhMm);
