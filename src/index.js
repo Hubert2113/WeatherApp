cd030d9a8d524c429fb1842e0396855bd1cf2ade
Hubert2113 <htumialiss@gmail.com>
2022-04-19 21:40:32 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import './sass/main.scss';
 import './partials-js/chart';
-import {getCityWeather, citiesData} from './partials-js/data_fetch';
+import {getCityWeather, citiesData, getDefaultCityData} from './partials-js/data_fetch';
 import axios from 'axios';
 
 let defaultCity = [];
 
-document.addEventListener("DOMContentLoaded", async () => {
-    await axios.get("https://api.openweathermap.org/data/2.5/forecast?lat=52.2319581&lon=21.0067249&units=metric&appid=86882c431a5c1fa03f48939e3b313043")
-    .then((response) => {
-        defaultCity.push(response);
-    })
-    .catch((error) => {
-        return error;
-    });
-});
+document.addEventListener("DOMContentLoaded", getDefaultCityData());
 
 $searchInput.addEventListener("input", async (ev) => {
     $searchBtn.addEventListener("click", await getCityWeather(ev));
