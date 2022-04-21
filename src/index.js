f4bfada93682d89f19154f817a5eb7e830a14360
Kamil Przebieracz <dmjs23@gmail.com>
2022-04-21 21:03:26 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import './sass/main.scss';
-import './partials-js/chart';
+// import './partials-js/chart';
 import './partials-js/weather';
-import {getCityWeather, citiesData, getDefaultCityData, getInputValue,
-    defaultCity, inputValue,} from './partials-js/data_fetch';
+import {
+  getCityWeather,
+  citiesData,
+  getDefaultCityData,
+  getInputValue,
+  defaultCity,
+  inputValue,
+} from './partials-js/data_fetch';
 import axios from 'axios';
-import { historyAddCity } from './partials-js/history_city'
+import { historyAddCity } from './partials-js/history_city';
 
-const $searchInput = document.querySelector("input.search-bar");
-const $submitBtn = document.querySelector("button.submit-btn");
+// const $searchInput = document.querySelector('input.search-bar');
+// const $submitBtn = document.querySelector('button.submit-btn');
 
-document.addEventListener("DOMContentLoaded", getDefaultCityData());
+document.addEventListener('DOMContentLoaded', getDefaultCityData);
 
 // $searchInput.addEventListener("input", getInputValue(ev));
 // $submitBtn.addEventListener("click", await getCityWeather(ev));
