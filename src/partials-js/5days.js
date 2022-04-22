d60dd3be231e83c85a12785b8b4ab72a189d1189
Kamil Przebieracz <dmjs23@gmail.com>
2022-04-22 22:21:06 +0200

    Update 5days.js

--- src/partials-js/5days.js
+++ src/partials-js/5days.js
@@ -3,22 +3,15 @@ import { arrayOfKey } from './history_city';
 
 export function GenerateView5Days() {
   const $5DaysSection = document.querySelector('.Days5');
-  $5DaysSection.classList.remove('Days5_hiden');
-  //=====================kod na potencjalne strzałki==================
-  // const $5days_arrow = document.createElement('div');
-  // let $5days_arrowCode = `
-  //   <button class="5Days_arrow">
-  //     <
-  //   </button>`;
-  // $5days_arrow.innerHTML = $5days_arrowCode;
-  // $5DaysSection.append($5days_arrow);
-  //=====================kod na potencjalne strzałki==================
+  const $vievIsDone = document.querySelector('.Days5_viev');
 
-  arrayOfKey.forEach(el => {
-    for (let index = 0; index < 33; index += 8) {
-      const Days5 = document.createElement('div');
-      Days5.classList.add('Days5_viev');
-      const days5Array = `
+  if (!$vievIsDone) {
+    $5DaysSection.classList.remove('Days5_hiden');
+    arrayOfKey.forEach(el => {
+      for (let index = 0; index < 33; index += 8) {
+        const Days5 = document.createElement('div');
+        Days5.classList.add('Days5_viev');
+        const days5Array = `
       <div class="Days5_viev__week">
     <p>${loadStorage(el).list[index].dt_txt}</p>
     </div>
@@ -32,10 +25,20 @@ export function GenerateView5Days() {
     <p>${Math.round(loadStorage(el).list[index].main.temp_min)} &deg</p>
     <p>${Math.round(loadStorage(el).list[index].main.temp_max)} &deg</p>
     </div>`;
-      Days5.innerHTML = days5Array;
-      $5DaysSection.append(Days5);
-    }
-  });
+        Days5.innerHTML = days5Array;
+        $5DaysSection.append(Days5);
+      }
+    });
+  }
+  //=====================kod na potencjalne strzałki==================
+  // const $5days_arrow = document.createElement('div');
+  // let $5days_arrowCode = `
+  //   <button class="5Days_arrow">
+  //     <
+  //   </button>`;
+  // $5days_arrow.innerHTML = $5days_arrowCode;
+  // $5DaysSection.append($5days_arrow);
+  //=====================kod na potencjalne strzałki==================
 
   //=====================kod na potencjalne strzałki==================
   // const $5days_arrow2 = document.createElement('div');
