b6a1576d761dbb1001a891c4798a13c9ddbfdb8e
Kamil Przebieracz <dmjs23@gmail.com>
2022-04-24 21:25:39 +0200

    update

--- src/partials-js/history_city.js
+++ src/partials-js/history_city.js
@@ -1,50 +1,50 @@
-import { cityData } from '..';
+// import { cityData } from '..';
 import { saveStorage, loadStorage, removeStorage } from './storage';
 // import { } from "./data_fetch";
 
-
 export const arrayOfKey = [];
 
-
 export function historyAddCity(cityData) {
-  saveStorage(cityData.city.id, cityData);
+  saveStorage(cityData.city.name, cityData);
 }
 
-export function historyCityVieve() {
+export function historyCityVieve(reload) {
   //sprawdzenie długości local storage
   const storageLength = localStorage.length;
   // const arrayOfKey = [];
   for (let index = 0; index < storageLength; index++) {
     arrayOfKey.push(window.localStorage.key(index));
   }
-  GenerateViewHistory(arrayOfKey);
+  GenerateViewHistory(arrayOfKey, reload);
 }
 
 export function HistoryCityDelete(index) {
   removeStorage(index);
 }
 
-function GenerateViewHistory(arrayOfKey) {
-  const $historySection = document.querySelector('.history');
+function GenerateViewHistory(arrayOfKey , reload) {
+  const historySection = document.querySelector('.history');
+
   const history_arrow = document.createElement('div');
   let historyArowCode = `
     <button class="history_arrow">
       <
     </button>`;
   history_arrow.innerHTML = historyArowCode;
-  $historySection.append(history_arrow);
+  historySection.append(history_arrow);
 
   arrayOfKey.forEach(el => {
-    const history = document.createElement('div');
+    let history = document.createElement('div');
     history.classList.add('history_button');
 
-    const historyArray = `
+    let historyArray = null;
+    historyArray = `
     <a class="history_button_link" href="${el}">
       ${loadStorage(el).city.name}
     </a>`;
     history.innerHTML = historyArray;
 
-    $historySection.append(history);
+    historySection.append(history);
   });
   const history_arrow2 = document.createElement('div');
   historyArowCode = `
@@ -52,5 +52,9 @@ function GenerateViewHistory(arrayOfKey) {
       >
     </button>`;
   history_arrow2.innerHTML = historyArowCode;
-  $historySection.append(history_arrow2);
+  historySection.append(history_arrow2);
+  if (reload === 1)
+  {
+    location.reload();
+  }
 }
