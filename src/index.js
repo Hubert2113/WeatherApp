962c57dd522694476b335c12800b87c2ed843550
Kamil Przebieracz <dmjs23@gmail.com>
2022-04-25 23:14:03 +0200

    clean index and add history delete in button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './sass/main.scss';
 // import './partials-js/chart';
-import {mainWeather} from './partials-js/weather';
+import { mainWeather } from './partials-js/weather';
 import {
   getCityWeather,
   getDefaultCityData,
@@ -23,20 +23,16 @@ import { setTodayAsActive, set5DaysAsActive } from './partials-js/weather_button
 export let cityData = loadStorage('cityData');
 
 // definicje przycisków
-
 const $searchInput = document.querySelector('input.search-bar');
 const $submitBtn = document.querySelector('button.submit-btn');
 const $Days5Btn = document.querySelector('#Days5Btn');
 const $Days1Btn = document.querySelector('#today-btn');
+const todayBtn = document.querySelector('#today-btn');
+// definicje przycisków
 
+// dodawanie zdarzen
 $Days5Btn.addEventListener('click', GenerateView5Days);
 $Days1Btn.addEventListener('click', Day5Hiden);
-
-if(cityData)
-mainWeather();
-historyCityVieve(0);
-
-document.addEventListener('DOMContentLoaded', getDefaultCityData);
 $searchInput.addEventListener('input', getInputValue);
 $submitBtn.addEventListener('click', async ev => {
   ev.preventDefault();
@@ -44,26 +40,26 @@ $submitBtn.addEventListener('click', async ev => {
   historyAddCity(cityData);
   historyCityVieve(1);
 });
+$Days5Btn.addEventListener('click', GenerateView5Days);
+todayBtn.addEventListener('click', setTodayAsActive);
+$Days5Btn.addEventListener('click', set5DaysAsActive);
+// dodawanie zdarzen
 
-const todayBtn = document.querySelector('#today-btn');
-// dodanie event listenerów
+// podstawowe ładowanie danych
+if (cityData) mainWeather();
+historyCityVieve(0);
 
 //=========kod dzieku ktoremu jezeli jeszcze cityData nie jest dostępne
 //========= w local storage to przeładuje strone ======================
 if (!loadStorage('cityData')) {
   document.addEventListener('DOMContentLoaded', getDefaultCityData);
-
   setTimeout(() => {
     location.reload();
   }, 500);
 }
+
+// zegar na głównej stronie
 createHTMLMarkup(cityData);
 setInterval(() => timer(cityData), 1000);
-$Days5Btn.addEventListener('click', GenerateView5Days);
-todayBtn.addEventListener('click', setTodayAsActive);
-$Days5Btn.addEventListener('click', set5DaysAsActive);
-// historyAddCity(cityData);
-// historyCityVieve();
-// GenerateView5Days();
 
 console.log(cityData);
