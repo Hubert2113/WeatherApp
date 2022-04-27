581c23647d1203a562a9b3a2fc5c68ab0a897c4a
MartaCzerniak <czerniakmarta2406@gmail.com>
2022-04-27 12:37:17 +0200

    poprawa css
    wciąż nie działa mi live server i wszystko edytuję wspomagając się edytorem w przeglądarce, więc nie widzę jak to naprawdę wygląda. oby było spoko

--- src/partials-js/weather.js
+++ src/partials-js/weather.js
@@ -8,15 +8,19 @@ export function mainWeather() {
 
   const $todaySection = document.querySelector('.weather-container');
   const today_section = `
+  <div class="weather-txt-container">
     <p class="weather_section_city_name">${city_name}</p>
-    <p class="today_current_temp">${Math.round(current_temp)}&deg</p>
-    <div>
+    <p class="today_current_temp">${Math.round(current_temp)}</p>
+    <div class="min-max-container">
+    <div class="min-max-txt-div">
     <p class="today_temp_txt">min</p>
     <p class="today_temp_txt">max</p>
     </div>
-    <div>
+    <div class="min-max-value-div">
     <p class="today_temp_value">${Math.round(min_temp)} &deg</p>
     <p class="today_temp_value">${Math.round(max_temp)} &deg</p>
+    </div>
+    </div>
     `;
   $todaySection.innerHTML = today_section;
 }
